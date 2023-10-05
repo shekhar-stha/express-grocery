999aaddcc24d10df1c3487779b09bd0f93e698eb
Shekhar Shrestha <shekharr.shresthaa@gmail.com>
2023-10-05 21:31:58 +0545

    feat: updated products navigation 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import "../styles/scss/_global.scss"
+import '@fortawesome/fontawesome-svg-core/styles.css'
 export default function App({ Component, pageProps }) {
     return (
         <>
