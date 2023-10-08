57f47a6f3368f1aca496e7d6eda7d32599b19bc8
Shekhar Shrestha <shekharr.shresthaa@gmail.com>
2023-10-08 16:21:13 +0545

    feat: search filter

--- pages/search/[name].js
+++ pages/search/[name].js
@@ -5,12 +5,14 @@ import ProductCard from "../../components/products/productCard";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Link from "next/link";
+import Footer from "../../components/info/footer";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function Search() {
     const router = useRouter();
     const { name } = router.query;
-    console.log(name, "Search")
+    const [displayedProducts, setDisplayedProducts] = useState(12);
+
     const { data, error } = useSWR('/api/staticdata', fetcher);
 
     if (error) return <div>Error loading data</div>;
@@ -18,7 +20,12 @@ export default function Search() {
 
 
     const products = JSON.parse(data);
-    const searchProducts = products.filter((product) => product.name.toLowerCase().includes(name.toLowerCase()))
+    const searchProducts = products.filter((product) => product.name?.toLowerCase().includes(name?.toLowerCase()))
+    const shownProducts = searchProducts.slice(0, displayedProducts);
+
+    const handleViewMore = () => {
+        setDisplayedProducts(displayedProducts + 8);
+    };
     return (
         <>
             <Navbar />
@@ -26,9 +33,9 @@ export default function Search() {
                 <div className="container">
                     <p className="fs-22 text-capitalize fw-500">Search results for <strong>{name}</strong> <br />
                     </p>
-                    <p className="mt-3 fw-500">Showing {searchProducts?.length} results</p>
-                    <div className="products-container-row">
-                        {searchProducts?.map((product) => {
+                    <p className="mt-3 fw-500">Showing {shownProducts?.length} of {searchProducts?.length} results</p>
+                    <div className="products-container-row pb-5">
+                        {shownProducts?.map((product) => {
                             return (
                                 <Link href={`/product/${product?.id}`} key={product?.id}>
 
@@ -44,8 +51,16 @@ export default function Search() {
                             );
                         })}
                     </div>
+                    {searchProducts.length > displayedProducts && (
+                        <div className="d-flex">
+                            <button className="btn btn-primary mx-auto" onClick={handleViewMore}>
+                                View More
+                            </button>
+                        </div>
+                    )}
                 </div>
             </section>
+            <Footer />
         </>
     )
 }
